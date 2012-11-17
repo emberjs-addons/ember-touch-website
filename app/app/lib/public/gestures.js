e38d0fe5366f6f3eb51a8c482d63152d707615ee
Diego Muñoz Escalante <escalant3@gmail.com>
2012-11-17 10:58:04 -0500

    Added DoubleTap demo

--- app/app/lib/public/gestures.js
+++ app/app/lib/public/gestures.js
@@ -21,6 +21,22 @@ App.TapView = Em.View.extend({
 
 });
 
+App.DoubleTapView = Em.View.extend({
+  classNameBindings: ['isGreen'],
+  classNames: ['tap-view'],
+  isGreen: false,
+
+  tapOptions: {
+    numberOfTaps: 2,
+    delayBetweenTaps: 300
+  },
+
+  tapEnd: function () {
+    set(this, 'isGreen', !get(this, 'isGreen') );
+  }
+
+});
+
 App.TestView = Em.View.extend({
   templateName: 'test'
 });
