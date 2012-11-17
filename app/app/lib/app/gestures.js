29c1670a2b4259cc77d0087535e04bd1a545f3e9
Diego Muñoz Escalante <escalant3@gmail.com>
2012-11-17 09:13:28 -0500

    Double tap example

--- app/app/lib/app/gestures.js
+++ app/app/lib/app/gestures.js
@@ -20,4 +20,21 @@ App.TapView = Em.View.extend({
   }
 
 });
+
+App.DoubleTapView = Em.View.extend({
+  classNameBindings: ['isGreen'],
+  classNames: ['tap-view'],
+  isGreen: false,
+
+  tapOptions: {
+    numberOfTaps: 2,
+    delayBetweenTaps: 300
+  },
+
+  tapEnd: function () {
+    set(this, 'isGreen', !get(this, 'isGreen') );
+  }
+
+});
+
 App.initialize();
